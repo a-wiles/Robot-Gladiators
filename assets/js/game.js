7a1789a9e3194bb774e227005a36289b3bfb2fd7
Alex Wiles <alexandra.wiles6@gmail.com>
2022-02-14 21:34:29 -0500

    Starting Java

---
+++ assets/js/game.js
@@ -0,0 +1,13 @@
+// this creates a function named "Fight"
+
+function fight() {
+    window.alert("The fight has begun");
+}
+
+var playerName = window.prompt("What is your Robot's name?");
+// What is this?
+console.log(playerName);
+var name = "your name";
+console.log(name);
+
+//fight();
